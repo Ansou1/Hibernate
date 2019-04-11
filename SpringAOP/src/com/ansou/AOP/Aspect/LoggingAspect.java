1473f354cac3e299b6622970aa39397194f9a2e5
simon <simondaguenet81@gmail.com>
2019-04-11 09:56:39 +0900

    AOP condition

--- SpringAOP/src/com/ansou/AOP/Aspect/LoggingAspect.java
+++ SpringAOP/src/com/ansou/AOP/Aspect/LoggingAspect.java
@@ -2,13 +2,24 @@ package com.ansou.AOP.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution(public void com.ansou.AOP.DAO.AccountDAO.addAccount())")
+
+    @Pointcut("execution(public void com.ansou.AOP.DAO.AccountDAO.addAccount())")
+    private void forDaopackage() {}
+
+
+    @Before("forDaopackage()")
     public void beforeAddAccount() {
         System.out.println("This has been executed before AddAccount");
+    }
+
+    @Before("forDaopackage()")
+    public void performApiAnalytics() {
+        System.out.println("API analytics");
     }
 }
